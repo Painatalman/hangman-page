aecc4830264e58a910ccd6d0ce9c99c00d2cf3d2
Miguel Costa <maiquedi@gmail.com>
2020-08-24 01:23:28 +0100

    Updates

---
+++ precache-manifest.e6b084e03b2ab3cab09160dd479b5ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be1b1668f249557d713ebad6a2053cc",
+    "url": "/hangman-page/index.html"
+  },
+  {
+    "revision": "3db9a1dd8a78e60efc13",
+    "url": "/hangman-page/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "14db1066b8318d616058",
+    "url": "/hangman-page/static/js/2.d960751f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hangman-page/static/js/2.d960751f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db9a1dd8a78e60efc13",
+    "url": "/hangman-page/static/js/main.1e56e2f7.chunk.js"
+  },
+  {
+    "revision": "4e68e40a572992cbd744",
+    "url": "/hangman-page/static/js/runtime-main.c415fd7a.js"
+  },
+  {
+    "revision": "1cc90bfebbf2979c3acac516c9833707",
+    "url": "/hangman-page/static/media/page-head.1cc90bfe.png"
+  }
+]);
